5181f2d5611b520266ad3480e2a07746f7ddc6db
Matías Aquino <87386473+MatyCrazy1996@users.noreply.github.com>
2023-01-28 16:08:48 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,27 @@
+import math
+
 def calculator():
-    operation = input("What operation would you like to perform (+, -, *, /): ")
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    if operation == '+':
-        print(num1 + num2)
-    elif operation == '-':
-        print(num1 - num2)
-    elif operation == '*':
-        print(num1 * num2)
-    elif operation == '/':
-        print(num1 / num2)
+    operation = input("What operation would you like to perform (sqrt, log, log10, sin, cos, tan, asin, acos, atan): ")
+    num = float(input("Enter the number: "))
+    if operation == 'sqrt':
+        print(math.sqrt(num))
+    elif operation == 'log':
+        base = float(input("Enter the base of the logarithm: "))
+        print(math.log(num, base))
+    elif operation == 'log10':
+        print(math.log10(num))
+    elif operation == 'sin':
+        print(math.sin(num))
+    elif operation == 'cos':
+        print(math.cos(num))
+    elif operation == 'tan':
+        print(math.tan(num))
+    elif operation == 'asin':
+        print(math.asin(num))
+    elif operation == 'acos':
+        print(math.acos(num))
+    elif operation == 'atan':
+        print(math.atan(num))
     else:
         print("Invalid operator")
 
